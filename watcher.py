019b508754343dca75e2aab27ddf0851c94121a4
tmurray19 <me@taidghmurray.ie>
2019-08-13 10:48:13 +0100

    Syntax fix

--- watcher.py
+++ watcher.py
@@ -98,7 +98,7 @@ if __name__ == '__main__':
 
     log_file_name = os.path.join(
         Config.LOGS_LOCATION,
-        Config.WATCHER_LOGs, 
+        Config.WATCHER_LOG, 
         datetime.now().strftime("%Y.%m.%d-%H-%M-%S") + "_render_watcher_instance.log"
     )
 
