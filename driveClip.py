e2a3432031cb90b5532f77e0fe23246dcf545efb
tmurray19 <me@taidghmurray.ie>
2019-08-13 13:32:22 +0100

    Updating bitrate

--- driveClip.py
+++ driveClip.py
@@ -328,7 +328,7 @@ def render_video(user, compress_render=False):
                 ), 
                 threads=8,
                 preset="ultrafast",
-                bitrate="250k",
+                bitrate="350k",
 
         )
     else:
