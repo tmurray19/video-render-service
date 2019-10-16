86e05bb864cedacca9faee254fbf84e7d9af1577
Ubuntu <sherpa-render@sherpa-render-full.y5mg0edjfjgengnom3kemaitgb.fx.internal.cloudapp.net>
2019-10-16 13:51:08 +0000

    Changing watcher for full VM work

--- intro_config.py
+++ intro_config.py
@@ -11,9 +11,9 @@ class introConfig(object):
     #VIDEOS_LOCATION = os.environ.get('VIDEOS_LOCATION') or "/videosherpa/videos/"
     #QUE_LOCATION = os.environ.get('QUE_LOCATION') or "/videosherpa/videos/templateque/"
 
-    LOGS_LOCATION = os.environ.get('DIR_LOCATION') or "/mnt/csae48d5df47deax41bcxbaa/logs/introGen"
-    VIDEOS_LOCATION = os.environ.get('VIDEOS_LOCATION') or "/mnt/csae48d5df47deax41bcxbaa/videos/"
-    QUE_LOCATION = os.environ.get('QUE_LOCATION') or "/mnt/csae48d5df47deax41bcxbaa/videos/templateque/"
+    LOGS_LOCATION = os.environ.get('DIR_LOCATION') or "/mnt/csae48d5df47deax41bcxbaa/SherpaVideos/logs/introGen"
+    VIDEOS_LOCATION = os.environ.get('VIDEOS_LOCATION') or "/mnt/csae48d5df47deax41bcxbaa/SherpaVideos/Videos/"
+    QUE_LOCATION = os.environ.get('QUE_LOCATION') or "/mnt/csae48d5df47deax41bcxbaa/SherpaVideos/Videos/templateque/"
 
     SERVICENAME = os.environ.get('SERVICENAME') or "Sherpa-Templates-Render"
 
